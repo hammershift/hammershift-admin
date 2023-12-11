27626e61eca1d0c1f800923418e3272513268a6f
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-12-11 19:48:08 +0800

    commit changes

--- src/app/ui/dashboard/search/Search.tsx
+++ src/app/ui/dashboard/search/Search.tsx
@@ -4,7 +4,7 @@ import magnifyingGlass from '@/../public/images/magnifying-glass.svg';
 
 const Search = ({placeholder} : {placeholder: string}) => {
   return (
-        <div className='tw-self-center '>
+        <div className='tw-self-center'>
           <div className='tw-bg-[#fff]/20 tw-h-auto tw-flex tw-px-2 tw-py-1.5 tw-rounded tw-gap-1'>
             <Image src={magnifyingGlass} alt='magnifying glass' width={20} height={20}/>
             <input placeholder={`Search for ${placeholder}`} className='tw-bg-transparent focus:tw-outline-none'/>
