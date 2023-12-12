b660c7a182adda0b9b69d840c4fc03a865e91804
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-12-12 15:32:58 +0800

    fix: changed return value for admin route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export const dynamic = 'force-dynamic'
 
 export async function GET(req: NextRequest) {
-    return null;
+    return {message: "Hello World!"};
 } 
