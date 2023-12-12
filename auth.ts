6120a6e1d65759524a66cad6751a734cfe3d059a
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-12-12 09:32:09 +0800

    fix: continued with next auth

--- auth.ts
+++ auth.ts
@@ -2,17 +2,25 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+// import bcrypt from 'bcrypt';
 
 
-// async function getUser(email: string): Promise<User | undefined> {
-//   try {
-//     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-//     return user.rows[0];
-//   } catch (error) {
-//     console.error('Failed to fetch user:', error);
-//     throw new Error('Failed to fetch user.');
-//   }
-// }
+
+async function getUser(email: string) {
+  try {
+    const res = await fetch('http://localhost:3000/api/user');
+    if (res) {
+      const user = await res.json();
+      return user;
+    } else {
+      return null
+      console.log({message: 'User not found'})
+    }
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -22,6 +30,18 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
+
+          if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+
+          //  const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+          // if (passwordsMatch) return user;
+        }
+        console.log('Invalid credentials');
+        return null;
       },
     }),
 ],
