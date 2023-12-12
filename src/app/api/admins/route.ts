a8c90467ba8552230c19832ccb6df4bb94f8faae
rrmgarcia <ronaldravengarcia@gmail.com>
2023-12-12 16:53:46 +0800

    fixed admin route conforme to develop branch

--- src/app/api/admins/route.ts
+++ src/app/api/admins/route.ts
@@ -1,16 +1,16 @@
 // import connectToDB from "@/lib/mongoose";
 import connectToDB from "@/app/lib/mongoose";
-import Auctions from "@/app/models/auction.model";
+import Auctions from "@/app/models/admin.model";
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
   try {
     await connectToDB();
-    const auction_id = req.nextUrl.searchParams.get('auction_id');
-    const offset = Number(req.nextUrl.searchParams.get('offset')) || 0;
-    const limit = Number(req.nextUrl.searchParams.get('limit'));
+    const auction_id = req.nextUrl.searchParams.get("auction_id");
+    const offset = Number(req.nextUrl.searchParams.get("offset")) || 0;
+    const limit = Number(req.nextUrl.searchParams.get("limit"));
 
     // api/cars?auction_id=213123 to get a single car
     if (auction_id) {
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
     const cars = await Auctions.find().limit(limit).skip(offset);
     return NextResponse.json({ total: cars.length, cars: cars });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return NextResponse.json({ message: "Internal server error" });
   }
 }
