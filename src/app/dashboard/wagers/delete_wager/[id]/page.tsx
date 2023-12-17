27c49506c3a0bcc9d95d564518ea8485418861fb
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-12-17 19:51:47 +0800

    feat: integrated delete api to delete page of wagers

--- src/app/dashboard/wagers/delete_wager/[id]/page.tsx
+++ src/app/dashboard/wagers/delete_wager/[id]/page.tsx
@@ -1,23 +1,56 @@
-import React from 'react'
-import Link from 'next/link'
+"use client";
+import { getOneWager, editWagerWithId } from "@/app/lib/getWagers";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const DeleteWager = ({ params }: { params: { id: string } }) => {
-      const ID = params.id;
-  return (
-    <div>
-        <div>DeleteWager</div>
-        <div>Are you sure you want to delete wager:</div>
+    const [data, setData] = useState<any>(null);
+    const ID = params.id;
+    const router = useRouter();
 
-        <div className='tw-flex tw-gap-4 tw-mt-4'>
-            <Link href={`/dashboard/wagers`}>
-                <button className='btn-transparent-white'>back</button>
-            </Link>
-            <Link href={`/dashboard/wagers/delete_wager/${ID}`}>
-                <button className='btn-transparent-red'>DELETE WAGER</button> 
-            </Link>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getOneWager(ID);
+                console.log(data);
+                if (data) {
+                    setData(data);
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const res = await editWagerWithId(ID, { isActive: false });
+        if (res) {
+            alert("Wager Edit Successful");
+            router.push("/dashboard/wagers");
+        } else {
+            alert("User Edit Failed");
+        }
+    };
+
+    return (
+        <div>
+            <div>DeleteWager</div>
+            <div>Are you sure you want to delete wager:</div>
+            <div>{data && JSON.stringify(data)}</div>
+
+            <div className="tw-flex tw-gap-4 tw-mt-4">
+                <Link href={`/dashboard/wagers`}>
+                    <button className="btn-transparent-white">back</button>
+                </Link>
+                <button className="btn-transparent-red" onClick={handleSubmit}>
+                    DELETE WAGER
+                </button>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default DeleteWager
+export default DeleteWager;
