2f86dace863d05ad67ad01babeab4eb4c25198c7
rrmgarcia <ronaldravengarcia@gmail.com>
2023-12-20 11:56:04 +0800

    feat: improved ui for show_wager page

--- src/app/dashboard/auctions/page.tsx
+++ src/app/dashboard/auctions/page.tsx
@@ -23,7 +23,7 @@ const Auctions = () => {
 
   const fetchData = async () => {
     try {
-      const data = await getCarsWithFilter({ limit: 100 });
+      const data = await getCarsWithFilter({ limit: 21 });
 
       if (data && "cars" in data) {
         console.log(data);
