f95f2a930a2f271da8129bd12abf3670428e2ef0
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-01-03 21:40:18 +0800

    fix: added check for authorization for edit and delete wagers

--- src/app/dashboard/wagers/delete_wager/[id]/page.tsx
+++ src/app/dashboard/wagers/delete_wager/[id]/page.tsx
@@ -7,71 +7,73 @@ import { useRouter } from "next/navigation";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const DeleteWager = ({ params }: { params: { id: string } }) => {
-  const [wagerData, setData] = useState<any>({});
-  const ID = params.id;
-  const router = useRouter();
+    const [wagerData, setData] = useState<any>({});
+    const ID = params.id;
+    const router = useRouter();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getOneWager(ID);
-        console.log(data);
-        if (data) {
-          setData(data);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getOneWager(ID);
+                console.log(data);
+                if (data) {
+                    setData(data);
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const res = await editWagerWithId(ID, { isActive: false });
+        if (res) {
+            alert("Wager Deleted");
+            router.push("/dashboard/wagers");
+        } else {
+            alert("Unauthorized Wager Delete");
         }
-      } catch (error) {
-        console.error("Error:", error);
-      }
     };
-    fetchData();
-  }, []);
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const res = await editWagerWithId(ID, { isActive: false });
-    if (res) {
-      alert("Wager Edit Successful");
-      router.push("/dashboard/wagers");
-    } else {
-      alert("User Edit Failed");
-    }
-  };
-
-  return (
-    <div className="section-container tw-mt-4">
-      <Link href={`/dashboard/wagers`}>
-        <ArrowBackIcon />
-      </Link>
-      <h2 className="tw-font-bold tw-m-4 tw-text-yellow-500">DeleteWager</h2>
-      <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
-        <h4>WAGER ID:</h4>
-        <p>{wagerData._id}</p>
-      </div>
-      <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
-        <h4>PRICE GUESSED:</h4>
-        <p>${wagerData.priceGuessed}</p>
-      </div>
-      <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
-        <h4>WAGER AMOUNT:</h4>
-        <p>${wagerData.wagerAmount}</p>
-      </div>
-      <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
-        <h4>USERNAME:</h4>
-        <p>{wagerData && wagerData.user && wagerData.user.username}</p>
-      </div>
-      <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
-        <h4>USER ID:</h4>
-        <p>{wagerData && wagerData.user && wagerData.user._id}</p>
-      </div>
-      <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
-        <h4>AUCTION ID:</h4>
-        <p>{wagerData.auctionID}</p>
-      </div>
-      <button className="btn-transparent-red" onClick={handleSubmit}>
-        DELETE WAGER
-      </button>
-    </div>
-  );
+    return (
+        <div className="section-container tw-mt-4">
+            <Link href={`/dashboard/wagers`}>
+                <ArrowBackIcon />
+            </Link>
+            <h2 className="tw-font-bold tw-m-4 tw-text-yellow-500">
+                DeleteWager
+            </h2>
+            <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
+                <h4>WAGER ID:</h4>
+                <p>{wagerData._id}</p>
+            </div>
+            <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
+                <h4>PRICE GUESSED:</h4>
+                <p>${wagerData.priceGuessed}</p>
+            </div>
+            <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
+                <h4>WAGER AMOUNT:</h4>
+                <p>${wagerData.wagerAmount}</p>
+            </div>
+            <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
+                <h4>USERNAME:</h4>
+                <p>{wagerData && wagerData.user && wagerData.user.username}</p>
+            </div>
+            <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
+                <h4>USER ID:</h4>
+                <p>{wagerData && wagerData.user && wagerData.user._id}</p>
+            </div>
+            <div className="tw-flex tw-justify-between tw-w-1/3 tw-mx-4 tw-my-2">
+                <h4>AUCTION ID:</h4>
+                <p>{wagerData.auctionID}</p>
+            </div>
+            <button className="btn-transparent-red" onClick={handleSubmit}>
+                DELETE WAGER
+            </button>
+        </div>
+    );
 };
 
 export default DeleteWager;
