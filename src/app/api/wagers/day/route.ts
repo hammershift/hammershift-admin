bf782c8f657ae1ebfc59241c81bbac4c64ac0188
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-01-14 22:46:06 +0800

    fix: created api to get wagers on a specific date

---
+++ src/app/api/wagers/day/route.ts
@@ -0,0 +1,59 @@
+import clientPromise from "@/app/lib/mongoDB";
+import connectToDB from "@/app/lib/mongoose";
+import Wagers from "@/app/models/wager.model";
+import { NextRequest, NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+
+export const dynamic = "force-dynamic";
+
+// /api/wagers/weekly
+export async function GET(req: NextRequest) {
+    try {
+        const client = await clientPromise;
+        const db = client.db();
+        // await connectToDB();
+        const date: string | null = req.nextUrl.searchParams.get("date");
+
+        // date string for start of day
+        const startOfToday = new Date(date as string);
+        startOfToday.setHours(0, 0, 0, 0);
+
+        //date string for end of day
+        const endOfToday = new Date(date as string);
+        endOfToday.setHours(23, 59, 59, 999);
+
+        // api/wagers to get all wagers
+        const wagers = await db
+            .collection("wagers")
+            .find({
+                createdAt: {
+                    $gte: startOfToday,
+                    $lt: endOfToday,
+                },
+            })
+            .toArray();
+
+        // To add {isActive: true} to the query
+        // const wagers = await db
+        //     .collection("wagers")
+        //     .find({ $and: [query, { isActive: true }] });
+
+        if (wagers) {
+            return NextResponse.json(
+                { total: wagers.length, wagers: wagers },
+                { status: 200 }
+            );
+        } else {
+            return NextResponse.json(
+                { message: "Cannot find Wagers" },
+                { status: 404 }
+            );
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
