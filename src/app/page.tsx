5628abbb1a413bdf09f17d56103f450ff1b4c22b
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-01-18 20:02:08 +0800

    fix: redirect user if already logged in to dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import Image from "next/image";
 import LoginPage from "./ui/login/loginPage/LoginPage";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="tw-flex tw-justify-center tw-items-center tw-pt-64">
-      <LoginPage />
-    </div>
-  );
+    const { data: session } = useSession();
+    useEffect(() => {
+        if (session) {
+            redirect("/dashboard");
+        }
+    }, [session]);
+
+    return (
+        <div className="tw-flex tw-justify-center tw-items-center tw-pt-64">
+            <LoginPage />
+        </div>
+    );
 }
