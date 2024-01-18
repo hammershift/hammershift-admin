3df46e32aa7e41d051d4bdb6be57e5ac1cb566b8
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-01-18 21:14:12 +0800

    fix: adjusted next auth to have role in the sessions

--- src/app/types/adminTypes.ts
+++ src/app/types/adminTypes.ts
@@ -1,11 +1,12 @@
 export interface Admin {
-  _id?: string;
-  username: string;
-  password?: string;
-  image?: string;
+    _id?: string;
+    username: string;
+    password?: string;
+    image?: string;
+    role?: string;
 }
 
 export interface Credentials {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
