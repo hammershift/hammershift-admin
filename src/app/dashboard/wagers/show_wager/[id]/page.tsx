a06c31bb7e818dc713491fce76f288786ffde959
rrmgarcia <ronaldravengarcia@gmail.com>
2024-01-23 10:05:54 +0800

    feat: implemented role based access by restricting actions depending on admin role. Improved UI for logout page, minor styling fix in edit wagers and users page

--- src/app/dashboard/wagers/show_wager/[id]/page.tsx
+++ src/app/dashboard/wagers/show_wager/[id]/page.tsx
@@ -4,10 +4,13 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useSession } from "next-auth/react";
 
 const ShowWager = ({ params }: { params: { id: string } }) => {
-  const [data, setData] = useState<any>(null);
+  const [wagerData, setWagerData] = useState<any>(null);
   const ID = params.id;
+
+  const { data } = useSession();
 
   // fetches data from the database
   useEffect(() => {
@@ -16,7 +19,7 @@ const ShowWager = ({ params }: { params: { id: string } }) => {
         const data = await getOneWager(ID);
         console.log(data);
         if (data) {
-          setData(data);
+          setWagerData(data);
         }
       } catch (error) {
         console.error("Error:", error);
@@ -34,41 +37,45 @@ const ShowWager = ({ params }: { params: { id: string } }) => {
       <div className="tw-flex tw-flex-col">
         <div className="tw-flex tw-justify-between tw-w-1/2 tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>WAGER ID:</h4>
-          <p className="tw-px-3">{data && data._id}</p>
+          <p className="tw-px-3">{wagerData && wagerData._id}</p>
         </div>
         <div className="tw-flex tw-justify-between tw-w-1/2  tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>USER ID:</h4>
-          <p className="tw-px-3">{data && data.user._id}</p>
+          <p className="tw-px-3">{wagerData && wagerData.user._id}</p>
         </div>
         <div className="tw-flex tw-justify-between tw-w-1/2  tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>AUCTION ID:</h4>
-          <p className="tw-px-3">{data && data.auctionIdentifierId}</p>
+          <p className="tw-px-3">
+            {wagerData && wagerData.auctionIdentifierId}
+          </p>
         </div>
         <div className="tw-flex tw-justify-between tw-w-1/2  tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>Full Name:</h4>
-          <p className="tw-px-3">{data && data.user.fullName}</p>
+          <p className="tw-px-3">{wagerData && wagerData.user.fullName}</p>
         </div>
         <div className="tw-flex tw-justify-between tw-w-1/2  tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>Username:</h4>
-          <p className="tw-px-3">{data && data.user.username}</p>
+          <p className="tw-px-3">{wagerData && wagerData.user.username}</p>
         </div>
         <div className="tw-flex tw-justify-between tw-w-1/2  tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>Price Guessed:</h4>
-          <p className="tw-px-3">${data && data.priceGuessed}</p>
+          <p className="tw-px-3">${wagerData && wagerData.priceGuessed}</p>
         </div>
         <div className="tw-flex tw-justify-between tw-w-1/2  tw-mx-4 tw-my-2 max-lg:tw-w-full">
           <h4>Wager Amount:</h4>
-          <p className="tw-px-3">${data && data.wagerAmount}</p>
+          <p className="tw-px-3">${wagerData && wagerData.wagerAmount}</p>
         </div>
       </div>
-      <div className="tw-flex tw-gap-1 tw-justify-evenly tw-w-1/2 tw-m-4 max-lg:tw-w-full">
-        <Link href={`/dashboard/wagers/edit_wager/${ID}`}>
-          <button className="btn-transparent-white ">EDIT WAGER</button>
-        </Link>
-        <Link href={`/dashboard/wagers/delete_wager/${ID}`}>
-          <button className="btn-transparent-red">DELETE WAGER</button>
-        </Link>
-      </div>
+      {data?.user?.role !== "guest" && data?.user?.role !== "moderator" ? (
+        <div className="tw-flex tw-gap-1 tw-justify-evenly tw-w-1/2 tw-m-4 max-lg:tw-w-full">
+          <Link href={`/dashboard/wagers/edit_wager/${ID}`}>
+            <button className="btn-transparent-white ">EDIT WAGER</button>
+          </Link>
+          <Link href={`/dashboard/wagers/delete_wager/${ID}`}>
+            <button className="btn-transparent-red">DELETE WAGER</button>
+          </Link>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,20 +1,28 @@
-"use client"
-import React from 'react'
-import { useRouter } from 'next/navigation'
-import { signOut } from "next-auth/react"
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 const Logout = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <div className='tw-p-4'>
-        <div>Are you sure you want to logout?</div>
-        <div className='tw-flex tw-gap-4 '>
-            <button className='btn-transparent-white' onClick={() => signOut({redirect: true, callbackUrl: "/"})}>Yes</button>
-            <button className='btn-transparent-white' onClick={() => router.push('/dashboard')}>No</button>
+    <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-full">
+      <div className="section-container tw-flex tw-flex-col tw-justify-center tw-items-center tw-rounded-md">
+        <h2 className="tw-m-2">Are you sure you want to logout?</h2>
+        <div className="tw-flex tw-gap-4">
+          <button
+            className="tw-border-2 tw-border-yellow-400 tw-rounded-md tw-py-1 tw-px-3 tw-bg-yellow-400 tw-text-red-600 tw-font-bold"
+            onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
+          >
+            Logout
+          </button>
+          <button className="" onClick={() => router.push("/dashboard")}>
+            Cancel
+          </button>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Logout
+export default Logout;
