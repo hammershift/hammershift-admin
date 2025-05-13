ace39e177bec16fe677dd0a5f94c723fc8e1d326
Ramon Arca <ramonteodoroarca@gmail.com>
2025-05-13 14:37:22 +0800

    Add components needed for auction redesign

---
+++ src/app/ui/components/tabs.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Trigger, List, Content, Root } from "@radix-ui/react-tabs";
+import { cn } from "@/app/helpers/utils";
+
+interface IProps {
+  className?: string;
+  children?: React.ReactNode;
+  [x: string]: any;
+}
+const Tabs = Root;
+
+const TabsList = React.forwardRef<HTMLDivElement, IProps>(
+  ({ className, ...props }: IProps, ref) => (
+    <List
+      ref={ref}
+      className={cn(
+        "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+TabsList.displayName = List.displayName;
+
+const TabsTrigger = React.forwardRef<HTMLButtonElement, IProps>(
+  ({ className, ...props }: IProps, ref) => (
+    <Trigger
+      ref={ref}
+      className={cn(
+        "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow",
+        className
+      )}
+      value={""}
+      {...props}
+    />
+  )
+);
+TabsTrigger.displayName = Trigger.displayName;
+
+const TabsContent = React.forwardRef<HTMLDivElement, IProps>(
+  ({ className, ...props }: IProps, ref) => (
+    <Content
+      ref={ref}
+      className={cn(
+        "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+        className
+      )}
+      value={""}
+      {...props}
+    />
+  )
+);
+TabsContent.displayName = Content.displayName;
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
---
+++ src/app/ui/components/textarea.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import { cn } from "@/app/helpers/utils";
+
+interface IProps {
+  className?: string;
+  children?: React.ReactNode;
+  [x: string]: any;
+}
+const Textarea = React.forwardRef<HTMLTextAreaElement, IProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
