84782354ab1c600863214db773f98f8080946397
MaxChronicles <johanssontan12@gmail.com>
2025-05-16 17:12:56 +0800

    Small fix

--- src/app/dashboard/agents/page.tsx
+++ src/app/dashboard/agents/page.tsx
@@ -4,16 +4,19 @@ import { getAgents } from "@/app/lib/data";
 import { AgentData } from "@/app/lib/interfaces";
 import AgentsPage from "@/app/ui/dashboard/agentsPage/AgentsPage";
 import React, { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 const Agents = () => {
-  const [adminData, setAdminData] = useState<AgentData[]>([]);
+  const [agentData, setAgentData] = useState<AgentData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     try {
       const data = await getAgents();
 
       if (data && "agents" in data) {
-        setAdminData(data.agents as AgentData[]);
+        setAgentData(data.agents as AgentData[]);
+        setIsLoading(false);
       } else {
         console.error("Unexpected data structure:", data);
       }
@@ -26,7 +29,17 @@ const Agents = () => {
     fetchData();
   }, []);
 
-  return <AgentsPage agentData={adminData} fetchData={fetchData} />;
+  return (
+    <div className="section-container mt-4">
+      {isLoading ? (
+        <div className="flex justify-center items-center h-[592px]">
+          <BeatLoader color="#F2CA16" />
+        </div>
+      ) : (
+        <AgentsPage agentData={agentData} fetchData={fetchData} />
+      )}
+    </div>
+  );
 };
 
 export default Agents;
