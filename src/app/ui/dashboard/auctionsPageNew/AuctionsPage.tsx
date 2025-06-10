3108bf4b3828985aaabc6e1e6bf371bee6218c4c
Ramon Arca <ramonteodoroarca@gmail.com>
2025-06-10 21:39:30 +0800

    Filter ended auctions on query
    Change date display to use -1 of deadline

--- src/app/ui/dashboard/auctionsPageNew/AuctionsPage.tsx
+++ src/app/ui/dashboard/auctionsPageNew/AuctionsPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { formatDistanceToNow, isValid } from "date-fns";
+import { formatDistanceToNow, isValid, subDays } from "date-fns";
 import { useSession } from "next-auth/react";
 import { BeatLoader, ClipLoader } from "react-spinners";
 import {
@@ -67,7 +67,7 @@ import {
   DialogTrigger,
   DialogFooter,
 } from "@/app/ui/components/dialog";
-import { set } from "mongoose";
+
 interface AuctionsPageProps {
   auctionData: CarData[];
   currentPage: number;
@@ -195,7 +195,7 @@ const AuctionsPage: React.FC<AuctionsPageProps> = ({
     if (!dateString) return "No end date";
 
     try {
-      const endDate = new Date(dateString);
+      const endDate = subDays(new Date(dateString), 1);
 
       if (!isValid(endDate)) {
         return "Invalid date";
@@ -275,7 +275,7 @@ const AuctionsPage: React.FC<AuctionsPageProps> = ({
     }
     if (
       !auction.isActive &&
-      Date.parse(auction.deadline.toString()) < Date.now()
+      Date.parse(subDays(auction.deadline, 1).toString()) < Date.now()
     ) {
       alert("Deadline has passed for this auction");
       return;
