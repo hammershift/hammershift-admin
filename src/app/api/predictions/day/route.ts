5eaad8a5e5ae7ca82abaa444a0e732cf52bc709b
Ramon Arca <ramonteodoroarca@gmail.com>
2025-08-01 01:09:11 +0800

    Remove date stuff, just query prediction length
    More testing on route

--- src/app/api/predictions/day/route.ts
+++ src/app/api/predictions/day/route.ts
@@ -4,26 +4,28 @@ import Predictions from "@/app/models/prediction.model";
 export async function GET(req: NextRequest) {
   try {
     await connectToDB();
-    const date: string | null = req.nextUrl.searchParams.get("date");
-    console.log(`Fetching predictions for date: ${date}`);
-    const startOfToday = new Date(date as string);
-    startOfToday.setHours(0, 0, 0, 0);
+    const predictions = await Predictions.find({});
+    return NextResponse.json({ total: predictions.length }, { status: 200 });
+    // const date: string | null = req.nextUrl.searchParams.get("date");
+    // console.log(`Fetching predictions for date: ${date}`);
+    // const startOfToday = new Date(date as string);
+    // startOfToday.setHours(0, 0, 0, 0);
 
-    //date string for end of day
-    const endOfToday = new Date(date as string);
-    endOfToday.setHours(23, 59, 59, 999);
+    // //date string for end of day
+    // const endOfToday = new Date(date as string);
+    // endOfToday.setHours(23, 59, 59, 999);
 
-    const predictions = await Predictions.find({
-      createdAt: {
-        $gte: startOfToday,
-        $lt: endOfToday,
-      },
-    });
+    // const predictions = await Predictions.find({
+    //   createdAt: {
+    //     $gte: startOfToday,
+    //     $lt: endOfToday,
+    //   },
+    // });
 
-    return NextResponse.json(
-      { total: predictions.length, predictions: predictions },
-      { status: 200 }
-    );
+    // return NextResponse.json(
+    //   { total: predictions.length, predictions: predictions },
+    //   { status: 200 }
+    // );
   } catch (error) {
     console.error("Failed to fetch predictions:", error);
     return NextResponse.error();
