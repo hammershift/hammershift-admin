387fa119c7d4a05d4fa82ab0db30e705d655d203
Ramon Arca <ramonteodoroarca@gmail.com>
2025-08-01 01:01:40 +0800

    Remove test stuff

--- src/app/api/predictions/day/route.ts
+++ src/app/api/predictions/day/route.ts
@@ -3,36 +3,27 @@ import { NextRequest, NextResponse } from "next/server";
 import Predictions from "@/app/models/prediction.model";
 export async function GET(req: NextRequest) {
   try {
-    console.log("This is a test log from the failing route.");
-    return NextResponse.json({ message: "Hello from the test route!" });
+    await connectToDB();
+    const date: string | null = req.nextUrl.searchParams.get("date");
+    console.log(`Fetching predictions for date: ${date}`);
+    const startOfToday = new Date(date as string);
+    startOfToday.setHours(0, 0, 0, 0);
 
-    // await connectToDB();
-    // const date: string | null = req.nextUrl.searchParams.get("date");
-    // console.log(`Fetching predictions for date: ${date}`);
-    // const startOfToday = new Date(date as string);
-    // startOfToday.setHours(0, 0, 0, 0);
+    //date string for end of day
+    const endOfToday = new Date(date as string);
+    endOfToday.setHours(23, 59, 59, 999);
 
-    // //date string for end of day
-    // const endOfToday = new Date(date as string);
-    // endOfToday.setHours(23, 59, 59, 999);
+    const predictions = await Predictions.find({
+      createdAt: {
+        $gte: startOfToday,
+        $lt: endOfToday,
+      },
+    });
 
-    // const predictions = await Predictions.find({
-    //   createdAt: {
-    //     $gte: startOfToday,
-    //     $lt: endOfToday,
-    //   },
-    // });
-    // if (predictions) {
-    //   return NextResponse.json(
-    //     { total: predictions.length, predictions: predictions },
-    //     { status: 200 }
-    //   );
-    // } else {
-    //   return NextResponse.json(
-    //     { message: "Cannot find Predictions" },
-    //     { status: 404 }
-    //   );
-    // }
+    return NextResponse.json(
+      { total: predictions.length, predictions: predictions },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Failed to fetch predictions:", error);
     return NextResponse.error();
