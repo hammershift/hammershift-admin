209f8363483de19fcf372e8abc22262e4b45f77c
Ramon Arca <ramonteodoroarca@gmail.com>
2025-08-01 09:30:41 +0800

    Change route name

--- src/app/api/predictions/dayCount/route.ts
+++ src/app/api/predictions/dayCount/route.ts
@@ -8,7 +8,10 @@ export async function GET(req: NextRequest) {
     const date = req.nextUrl.searchParams.get("predictionDate");
 
     if (!date) {
-      return NextResponse.error();
+      return NextResponse.json(
+        { message: "No date provided" },
+        { status: 400 }
+      );
     }
     return NextResponse.json({ date: date });
     //date string for end of day
